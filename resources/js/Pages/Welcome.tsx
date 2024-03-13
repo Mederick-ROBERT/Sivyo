565996a5942da1d2eb827c45d14cf1c21bbc1c45
Mederick-ROBERT <mederick.robert@free.fr>
2024-03-13 16:49:30 +0100

    build : install of Laravel x inertia x react

---
+++ resources/js/Pages/Welcome.tsx
@@ -0,0 +1,11 @@
+import { Link, Head } from '@inertiajs/react';
+
+export default function Welcome() {
+
+    return (
+        <>
+            <Head title="Welcome" />
+            <p>Home</p>
+        </>
+    );
+}
---
+++ routes/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+
+
